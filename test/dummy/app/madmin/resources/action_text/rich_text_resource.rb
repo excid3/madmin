d6b095a5d525fec4ff78bc0067048eb97cd11680
Chris Oliver <excid3@gmail.com>
2020-09-10 23:14:01 -0500

    Add ActionText resource example

---
+++ test/dummy/app/madmin/resources/action_text/rich_text_resource.rb
@@ -0,0 +1,9 @@
+class ActionText::RichTextResource < Madmin::Resource
+  attribute :id
+  attribute :name
+  attribute :body
+  attribute :record_type
+  attribute :record_id
+  attribute :created_at
+  attribute :updated_at
+end
