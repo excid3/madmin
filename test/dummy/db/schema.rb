35394a83a092316d227864a0d93ebf6152f77e74
Chris Oliver <excid3@gmail.com>
2021-01-15 17:07:00 -0600

    Fix column type for mysql

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -34,8 +34,8 @@ ActiveRecord::Schema.define(version: 2020_09_11_160756) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint"record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
