835b194e1c03ecb2f57b7c7f7f938185fdfac935
Dhurba Baral <dhurba87@gmail.com>
2021-03-22 21:00:18 +0545

    Adds a html form template to be used by js

--- app/helpers/madmin/application_helper.rb
+++ app/helpers/madmin/application_helper.rb
@@ -13,5 +13,24 @@ module Madmin
       version += "-#{Rails::VERSION::PRE}" if Rails::VERSION::PRE
       version
     end
+
+    def link_to_add_association(name, resource:, form:, field:, html_options: {})
+      record = form.object.send(field.attribute_name).build
+
+      attribute = field.attribute_name.to_s.singularize
+
+      name = "#{name} #{attribute}"
+      html_options[:class]= "#{html_options[:class]} add_fields"
+      html_options[:'data-association-insert-template'] = CGI.escapeHTML(form_template(form, field, resource, record, attribute))
+
+      link_to(name, '#', html_options)
+    end
+
+    def form_template(form, field, resource, record, attribute)
+      form.fields_for field.attribute_name, child_index: "new_#{attribute}" do |nested_form|
+        render(partial: "./madmin/application/fields", locals: { resource: resource, record: record, form: nested_form })
+      end
+    end
+
   end
 end
