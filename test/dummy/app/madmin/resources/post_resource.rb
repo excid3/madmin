fdafb0a8921dfd7d06e2f6a436c2cd6da66177a0
Chris Oliver <excid3@gmail.com>
2021-04-22 18:07:27 -0500

    Update post resource

--- test/dummy/app/madmin/resources/post_resource.rb
+++ test/dummy/app/madmin/resources/post_resource.rb
@@ -2,10 +2,23 @@ class PostResource < Madmin::Resource
   # Attributes
   attribute :id, form: false
   attribute :title
+  attribute :comments_count, form: false
+  attribute :metadata
+  attribute :enum
+  attribute :tags
+  attribute :ratings
+  attribute :created_at, form: false
+  attribute :updated_at, form: false
   attribute :body, index: false
   attribute :image, index: false
   attribute :attachments, index: false
-  attribute :enum
 
-  scope :recent
+  # Associations
+  attribute :user
+  attribute :comments
+
+  # Uncomment this to customize the display name of records in the admin area.
+  # def self.display_name(record)
+  #   record.name
+  # end
 end
