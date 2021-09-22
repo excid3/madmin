e5fc13e947ef02394d7beca8005e0024c0bed31f
Jacob Daddario <jake.daddario@gmail.com>
2021-09-21 21:49:51 -0500

    Re-implemnted draw so that the routes file works and passes CI

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
-  draw(:madmin)
+  if Gem.loaded_specs["rails"].version >= Gem::Version.new(6.1)
+    draw(:madmin)
+  else
+    route_path = "#{Rails.root}/config/routes/madmin.rb"
+    instance_eval(File.read(route_path), route_path.to_s)
+  end
 end
