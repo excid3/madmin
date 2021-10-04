9da703f66317c0f99d740567a71c13ea7d049c17
Jiting <jiting@jtcat.com>
2021-10-04 15:44:46 +0800

    add pagy's default tailwind style

--- lib/madmin/engine.rb
+++ lib/madmin/engine.rb
@@ -3,6 +3,7 @@ module Madmin
     config.before_configuration do |app|
       app.config.autoload_paths << File.expand_path("app/madmin/resources", Rails.root)
       app.config.autoload_paths << File.expand_path("app/madmin/fields", Rails.root)
+      app.config.assets.precompile << "madmin_manifest.js"
     end
 
     config.to_prepare do
