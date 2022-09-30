f7dc55080fe0742f9591e447e8e8d105c498ade6
Chris Oliver <excid3@gmail.com>
2022-09-30 08:45:05 -0500

    Standardize

--- lib/madmin/fields/nested_has_many.rb
+++ lib/madmin/fields/nested_has_many.rb
@@ -3,7 +3,7 @@ module Madmin
     class NestedHasMany < Field
       DEFAULT_ATTRIBUTES = %w[_destroy id].freeze
       def nested_attributes
-        resource.attributes.reject { |name, attribute| skipped_fields.include?(name) }
+        resource.attributes.except(*skipped_fields)
       end
 
       def resource
