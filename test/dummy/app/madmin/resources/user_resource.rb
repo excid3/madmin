69c36506accdeede735f07c178e5557e572e9063
Chris Oliver <excid3@gmail.com>
2024-11-04 16:09:48 -0600

    Remove missing attribute from madmin

--- test/dummy/app/madmin/resources/user_resource.rb
+++ test/dummy/app/madmin/resources/user_resource.rb
@@ -19,8 +19,6 @@ class UserResource < Madmin::Resource
   attribute :avatar, index: false
   attribute :something, :string, index: false, form: false
 
-  attribute :missing
-
   # Associations
   attribute :posts, :nested_has_many, skip: %I[attachments]
   attribute :comments
