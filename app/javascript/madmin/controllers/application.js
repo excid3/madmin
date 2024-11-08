1f2d105cdd184ec9f7e2c9eb81f778720a466198
Luan Estradioto <luanestradioto@gmail.com>
2024-11-07 22:21:33 -0300

    fix: no flatpickr for filters

--- app/javascript/madmin/controllers/application.js
+++ app/javascript/madmin/controllers/application.js
@@ -7,11 +7,3 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
-
-// *** I dont think we need these anymore ************
-
-// import { Dropdown } from "tailwindcss-stimulus-components"
-// application.register("dropdown", Dropdown)
-
-// import StimulusFlatpickr from "flatpickr_controller"
-// application.register("flatpickr", StimulusFlatpickr)
