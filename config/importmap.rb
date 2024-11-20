513f69e5d946a70ce12cc004e4f7c49ca64666bd
Kaka Ruto <kr@kakaruto.com>
2024-11-20 16:36:06 +0000

    Fix TomSelect integration

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,8 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
-pin_all_from Madmin::Engine.root.join("app/javascript/madmin/controllers"), under: "controllers", to: "madmin/controllers"
+pin_all_from Madmin::Engine.root.join("app/javascript/madmin/controllers"), under: "controllers",
+                                                                            to: "madmin/controllers"
 
-pin "tom-select", to: "https://unpkg.com/tom-select@2/dist/esm/tom-select.complete.js"
+pin "tom-select", to: "https://ga.jspm.io/npm:tom-select@2.4.1/dist/js/tom-select.complete.js"
 pin "tailwindcss-stimulus-components", to: "https://unpkg.com/tailwindcss-stimulus-components@5/dist/tailwindcss-stimulus-components.module.js"
