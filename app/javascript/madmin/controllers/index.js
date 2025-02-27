223851415d97b7d8382ab50297d2a21125b37b46
Luan Estradioto <luanestradioto@gmail.com>
2025-02-27 19:30:35 -0300

    fix last merge conflicts

--- app/javascript/madmin/controllers/index.js
+++ app/javascript/madmin/controllers/index.js
@@ -2,8 +2,4 @@ import { application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-
-import Flatpickr from 'stimulus-flatpickr'
-application.register('flatpickr', Flatpickr)
-
 eagerLoadControllersFrom("controllers", application)
