7de1f8427d795c86626ec6fb180f8e778caa232d
Chris Oliver <excid3@gmail.com>
2025-08-15 13:39:13 -0500

    Refactor

--- lib/madmin/fields/has_many.rb
+++ lib/madmin/fields/has_many.rb
@@ -33,23 +33,26 @@ module Madmin
         true
       end
 
-      def paginated_value(record, params, request)
-        page_key = "#{attribute_name}_page"
-        paginate value(record), page: params[page_key].to_i, page_key: page_key, request: request
-      end
-
       if Gem::Version.new(Pagy::VERSION) >= Gem::Version.new("43.0.0.rc")
         include Pagy::Method
 
-        def paginate(value, page:, page_key:, request:)
-          pagy value, page: page, page_key: page_key, request: request
+        def paginated_value(record, params)
+          page_key = "#{attribute_name}_page"
+          request = {
+            query: {
+              "#{attribute_name}_page" => [params[page_key].to_i, 1].max
+            }
+          }
+          pagy value(record), page_key: page_key, request: request
+        rescue Pagy::OptionError
         end
       else
-        # Pagy 9 or earlier
         include Pagy::Backend
 
-        def paginate(value, page:, page_key:, request: nil)
-          pagy value, page: page, page_param: page_key
+        def paginated_value(record, params)
+          page_key = "#{attribute_name}_page"
+          page = [params[page_key].to_i, 1].max
+          pagy value(record), page: page, page_param: page_key
         rescue Pagy::OverflowError, Pagy::VariableError
           pagy value, page: 1, page_param: page_key
         end
