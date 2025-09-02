29abf92f5e5a9864b8cf2686c3497b50bcf1d9da
Chris Oliver <excid3@gmail.com>
2025-09-02 09:48:13 -0500

    Always use bigint for testing

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2025_08_18_152409) do
     t.string "name", null: false
     t.text "body"
     t.string "record_type", null: false
-    t.integer "record_id", null: false
+    t.bigint "record_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
@@ -33,9 +33,9 @@ ActiveRecord::Schema.define(version: 2025_08_18_152409) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
-    t.datetime "created_at", precision: nil, null: false
+    t.bigint "record_id", null: false
+    t.bigint"blob_id", null: false
+    t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
